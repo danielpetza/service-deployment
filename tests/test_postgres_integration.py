6f7da83ed24a3f2c236ba6a1203c3ff0a3e5473c
Daniel Petza <daniel01petza@gmail.com>
2026-05-11 18:04:24 +0200

    Aggiunta pipeline

--- tests/test_postgres_integration.py
+++ tests/test_postgres_integration.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 def client():
     config = {"SQLALCHEMY_DATABASE_URI":os.environ.get("DATABASE_URL"), "SQLALCHEMY_TRACK_MODIFICATIONS":False}
     app = create_app(config)
-    with app.test_client():
+    with app.app_context():
         db.create_all()
         yield 
         db.session.remove()
